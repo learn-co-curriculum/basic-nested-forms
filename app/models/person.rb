60889b9416628c2631efd1811eb5585fd2511451
github username <github email address>
2021-03-22 15:28:44 -0500

    test

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,36 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  accepts_nested_attributes_for :addresses
 
+  def index
+    @people = Person.all
+  end
+
+  def new
+    @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
+  end
+
+  def create
+    person = Person.create(person_params)
+    redirect_to people_path
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(
+      :name,
+      addresses_attributes: [
+        :street_address_1,
+        :street_address_2, 
+        :city,
+        :state,
+        :zipcode,
+        :address_type
+      ]
+    )
+    end
+  end
 end
